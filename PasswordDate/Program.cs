1d89141c2bae111673c9ec0f8587c9a0528d30d6
Jayson Rowe <jayson.rowe@gmail.com>
2011-12-19 22:44:24 -0500

    first commit

---
+++ PasswordDate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PasswordDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime newPass = DateTime.Now.AddDays(120);
+            Console.WriteLine(newPass.ToShortDateString());
+            Console.ReadLine();
+        }
+    }
+}
